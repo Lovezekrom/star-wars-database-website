ec2c83aa4eade8c8ce9bd61c472a749a7d811f8d
ezeck <ema161@uclive.ac.nz>
2023-10-01 22:25:19 +1300

    Add search icon

--- public_html/planets.php
+++ public_html/planets.php
@@ -5,6 +5,7 @@
     <title>Star Wars Database - Planets</title>
     <link rel="stylesheet" href="css/style.css" />
     <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 <body>
 <img src="img/star_wars_logo.png" width="10%" class="centre" alt="Star Wars logo"/>
@@ -23,7 +24,7 @@
 <form>
     <label for="search-bar"></label>
     <input placeholder="Search" type="text" id="search-bar" />
-    <input type="submit">
+    <button type="submit"><i class="material-icons">search</i></button>
 </form>
 
 <h1>Planets</h1>
