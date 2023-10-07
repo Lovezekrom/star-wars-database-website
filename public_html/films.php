28a17acc1e67f578ae3bd31826c28eb506e47117
Ezeckiel <ema161@uclive.ac.nz>
2023-10-07 23:08:50 +1300

    Put films in their own container

--- public_html/films.php
+++ public_html/films.php
@@ -27,26 +27,29 @@
 </form>
 
 <h1>Films</h1>
-
-<?php
-try {
-    $open_review_s_db = new PDO("sqlite:resources/star_wars.db");
-    $open_review_s_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die($e->getMessage());
-}
-
-try {
-    $res = $open_review_s_db->query("SELECT filmID, film_title, film_release_date, image_url FROM film");
-    while($row = $res->fetch(PDO::FETCH_ASSOC)) {
-        echo $row['film_title'] . "<br />";
-        echo "<a href='films.php?id=" . $row['filmID'] ."'><img height='300' src='" . $row['image_url'] . "' /></a><br />";
-        echo $row['film_release_date'] . "<br /><br />";
+<div class="grid-container">
+    <?php
+    try {
+        $open_review_s_db = new PDO("sqlite:resources/star_wars.db");
+        $open_review_s_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die($e->getMessage());
     }
-} catch (PDOException $e) {
-    die($e->getMessage());
-}
-?>
+
+    try {
+        $res = $open_review_s_db->query("SELECT filmID, film_title, film_release_date, image_url FROM film");
+        while($row = $res->fetch(PDO::FETCH_ASSOC)) {
+            echo "<div>";
+            echo $row['film_title'] . "<br />";
+            echo "<a href='films.php?id=" . $row['filmID'] ."'><img height='300' src='" . $row['image_url'] . "' /></a><br />";
+            echo $row['film_release_date'] . "<br /><br />";
+            echo "</div>";
+        }
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
+    ?>
+</div>
 
 </body>
 </html>
