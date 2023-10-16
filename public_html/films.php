205aabb3024f869d885a61e8eb6ba9c140781271
Ezeckiel <ema161@uclive.ac.nz>
2023-10-16 16:51:19 +1300

    Display producers and characters of the film

--- public_html/films.php
+++ public_html/films.php
@@ -41,7 +41,7 @@
             $stmt->execute();
 
             $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
+            $cat = 1;
             echo $result['film_title'];
         }
 
@@ -51,53 +51,87 @@
     ?>
 </h1>
 
-    <?php
+<?php
 
+try {
+    $open_review_s_db = new PDO("sqlite:resources/star_wars.db");
+    $open_review_s_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die($e->getMessage());
+}
+
+if (!isset($_GET['id'])) {
     try {
-        $open_review_s_db = new PDO("sqlite:resources/star_wars.db");
-        $open_review_s_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo '<div class="grid-container">';
+        $res = $open_review_s_db->query("SELECT filmID, film_title, film_release_date, image_url FROM film");
+        while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
+            echo "<div>";
+            echo $row['film_title'] . "<br />";
+            echo "<a href='films.php?id=" . $row['filmID'] . "'><img height='400' src='" . $row['image_url'] . "'/></a><br />";
+            echo $row['film_release_date'] . "<br/><br />";
+            echo "</div>";
+        }
+        echo "</div>";
     } catch (PDOException $e) {
         die($e->getMessage());
     }
+} else {
+    try {
+        echo '<div class="grid-container-2">';
+        $query = "
+            SELECT filmID, film_title, film_opening_crawl, film_director, film_release_date, image_url 
+            FROM film
+            WHERE filmID={$_GET['id']}";
 
-    if (!isset($_GET['id'])) {
-        try {
-            echo '<div class="grid-container">';
-            $res = $open_review_s_db->query("SELECT filmID, film_title, film_release_date, image_url FROM film");
-            while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
-                echo "<div>";
-                echo $row['film_title'] . "<br />";
-                echo "<a href='films.php?id=" . $row['filmID'] . "'><img height='400' src='" . $row['image_url'] . "'/></a><br />";
-                echo $row['film_release_date'] . "<br/><br />";
-                echo "</div>";
-            }
-            echo "</div>";
-        } catch (PDOException $e) {
-            die($e->getMessage());
-        }
-    } else {
-        try {
-            echo '<div class="grid-container-2">';
-            $query = "SELECT filmID, film_title, film_opening_crawl, film_director, film_release_date, image_url FROM film WHERE filmID=".$_GET['id'];
-            $res = $open_review_s_db->query($query);
-            while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
-                echo "<div id='opening-crawl'>";
-                echo $row['film_opening_crawl'];
-                echo "</div>";
+        // Film info
+        $result = $open_review_s_db->query($query);
+        $film_info = $result->fetch(PDO::FETCH_ASSOC);
+        echo "<div id='opening-crawl'>";
+        echo "<b>Description</b>";
+        echo "<br><br>";
+        echo $film_info['film_opening_crawl'];
+        echo "<br><br>";
 
-                echo "<div id='movie-info'>";
-                echo "<img height='400' src='" . $row['image_url'] . "'/><br />";
-                echo "Directed by " . $row['film_director'] . "<br>";
-                echo "Released on " . $row['film_release_date'] . "<br>";
-                echo "</div>";
-            }
-            echo "</div>";
-        } catch (PDOException $e) {
-            die($e->getMessage());
+        // Characters in the film
+        echo "<b>Characters</b>";
+        echo "<br><br>";
+        $get_people = "
+            SELECT distinct people.people_name 
+            FROM people, film_people, film 
+            WHERE film.filmID={$_GET['id']}
+            and film.filmID = film_people.filmID
+            and film_people.peopleID=people.peopleID";
+        $people_result = $open_review_s_db->query($get_people);
+        while ($film_people = $people_result->fetch(PDO::FETCH_ASSOC)) {
+            echo $film_people['people_name'] . "<br>";
         }
+        echo "</div>";
+
+        // Right panel
+        echo "<div id='movie-info'>";
+        echo "<img alt='poster' height='400' src='" . $film_info['image_url'] . "'/><br />";
+        echo "<b>Directed by</b><br>" . $film_info['film_director'] . "<br><br>";
+
+        // Producers of the film
+        echo "<b>Produced by</b><br>";
+        $get_producers = "
+            SELECT distinct producer.producer_name 
+            FROM producer, film_producer, film 
+            WHERE film.filmID={$_GET['id']}
+            and film.filmID = film_producer.filmID
+            and film_producer.producerID=producer.producerID";
+        $producers_result = $open_review_s_db->query($get_producers);
+        while ($film_producer = $producers_result->fetch(PDO::FETCH_ASSOC)) {
+            echo $film_producer['producer_name'] . "<br>";
+        }
+        echo "<br>";
+        echo "<b>Released on</b><br>" . $film_info['film_release_date'] . "<br>";
+        echo "</div>";
+        echo "</div>";
+    } catch (PDOException $e) {
+        die($e->getMessage());
     }
-    ?>
-
+}
+?>
 </body>
 </html>
-
