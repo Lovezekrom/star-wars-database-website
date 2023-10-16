5fcdce7a0af41eb150234eda773c0cb927f1fbf6
Ezeckiel <ema161@uclive.ac.nz>
2023-10-16 14:47:46 +1300

    Add pages for each film. Display relevant information

--- public_html/films.php
+++ public_html/films.php
@@ -26,9 +26,33 @@
     <button type="submit"><i class="material-icons">search</i></button>
 </form>
 
-<h1>Films</h1>
-<div class="grid-container">
+<h1>
     <?php
+    try {
+        $open_review_s_db = new PDO("sqlite:resources/star_wars.db");
+        $open_review_s_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        if (!isset($_GET['id'])) {
+            echo 'Films';
+        } else {
+            $query = "SELECT film_title FROM film WHERE filmID=:id";
+            $stmt = $open_review_s_db->prepare($query);
+            $stmt->bindParam(':id', $_GET['id'], PDO::PARAM_INT);
+            $stmt->execute();
+
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            echo $result['film_title'];
+        }
+
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
+    ?>
+</h1>
+
+    <?php
+
     try {
         $open_review_s_db = new PDO("sqlite:resources/star_wars.db");
         $open_review_s_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -36,20 +60,43 @@
         die($e->getMessage());
     }
 
-    try {
-        $res = $open_review_s_db->query("SELECT filmID, film_title, film_release_date, image_url FROM film");
-        while($row = $res->fetch(PDO::FETCH_ASSOC)) {
-            echo "<div>";
-            echo $row['film_title'] . "<br />";
-            echo "<a href='films.php?id=" . $row['filmID'] ."'><img height='400' src='" . $row['image_url'] . "' /></a><br />";
-            echo $row['film_release_date'] . "<br /><br />";
+    if (!isset($_GET['id'])) {
+        try {
+            echo '<div class="grid-container">';
+            $res = $open_review_s_db->query("SELECT filmID, film_title, film_release_date, image_url FROM film");
+            while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
+                echo "<div>";
+                echo $row['film_title'] . "<br />";
+                echo "<a href='films.php?id=" . $row['filmID'] . "'><img height='400' src='" . $row['image_url'] . "'/></a><br />";
+                echo $row['film_release_date'] . "<br/><br />";
+                echo "</div>";
+            }
             echo "</div>";
+        } catch (PDOException $e) {
+            die($e->getMessage());
         }
-    } catch (PDOException $e) {
-        die($e->getMessage());
+    } else {
+        try {
+            echo '<div class="grid-container-2">';
+            $query = "SELECT filmID, film_title, film_opening_crawl, film_director, film_release_date, image_url FROM film WHERE filmID=".$_GET['id'];
+            $res = $open_review_s_db->query($query);
+            while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
+                echo "<div id='opening-crawl'>";
+                echo $row['film_opening_crawl'];
+                echo "</div>";
+
+                echo "<div id='movie-info'>";
+                echo "<img height='400' src='" . $row['image_url'] . "'/><br />";
+                echo "Directed by " . $row['film_director'] . "<br>";
+                echo "Released on " . $row['film_release_date'] . "<br>";
+                echo "</div>";
+            }
+            echo "</div>";
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
     }
     ?>
-</div>
 
 </body>
 </html>
