59520c03ba9adb1154eaee4902529305854e4f26
ezeck <ema161@uclive.ac.nz>
2023-10-22 12:47:04 +1300

    All fields need to filled in order to submit

--- public_html/new_film.php
+++ public_html/new_film.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Star Wars Database - People</title>
+    <script src="js/script.js"></script>
     <link rel="stylesheet" href="css/stylesheet.css" />
     <link rel="icon" type="image/x-icon" href="img/favicon.ico">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
@@ -44,37 +45,37 @@
     <form class="row gx-5" action="process_film.php" method="post">
         <div class="col-md-4 col-xs-12 mb-3">
             <label for="inputTitle" class="form-label text-white">Film title</label>
-            <input type="text" class="form-control" id="inputTitle" name="inputTitle" placeholder="Enter film's title">
+            <input type="text" class="form-control" id="inputTitle" name="inputTitle" placeholder="Enter film's title" required>
         </div>
 
         <div class="col-md-4 col-xs-12 mb-3">
             <label for="inputFirstName" class="form-label text-white">Director first name</label>
-            <input type="text" class="form-control" id="inputFirstName" name="inputFirstName" placeholder="Enter director's first name">
+            <input type="text" class="form-control" id="inputFirstName" name="inputFirstName" placeholder="Enter director's first name" required>
         </div>
 
         <div class="col-md-4 col-xs-12 mb-3">
             <label for="inputLastName" class="form-label text-white">Director last name</label>
-            <input type="text" class="form-control" id="inputLastName" name="inputLastName" placeholder="Enter director's last name">
+            <input type="text" class="form-control" id="inputLastName" name="inputLastName" placeholder="Enter director's last name" required>
         </div>
 
         <div class="col-md-12 col-xs-12 mb-3">
             <label for="inputOpening" class="form-label text-white">Opening crawl</label>
-            <textarea class="form-control" id="inputOpening" name="inputOpening" rows="5" placeholder="Enter the opening crawl"></textarea>
+            <textarea class="form-control" id="inputOpening" name="inputOpening" rows="5" placeholder="Enter the opening crawl" required></textarea>
         </div>
 
         <div class="col-md-4 col-xs-12 mb-3">
             <label for="inputEpisodeNum" class="form-label text-white">Episode number</label>
-            <input type="number" class="form-control" id="inputEpisodeNum" name="inputEpisodeNum" placeholder="Enter the episode number">
+            <input type="number" class="form-control" id="inputEpisodeNum" name="inputEpisodeNum" placeholder="Enter the episode number" required>
         </div>
 
         <div class="col-md-4 col-xs-12 mb-3">
             <label for="inputDate" class="form-label text-white">Release date</label>
-            <input type="date" class="form-control" id="inputDate" name="inputDate">
+            <input type="date" class="form-control" id="inputDate" name="inputDate" required>
         </div>
 
         <div class="col-md-4 col-xs-12 mb-3">
             <label for="inputImageLink" class="form-label text-white">Film poster URL</label>
-            <input type="url" class="form-control" id="inputImageLink" name="inputImageLink" placeholder="Enter poster URL">
+            <input type="url" class="form-control" id="inputImageLink" name="inputImageLink" placeholder="Enter poster URL" required>
         </div>
 
         <div class="col-12 col-md-12 mt-3">
